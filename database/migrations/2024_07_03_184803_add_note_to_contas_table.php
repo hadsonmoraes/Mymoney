38c720f95e346bc9fa8c8ae34c663d15b5d4e4b3
hadson moraes <hadsonmartins10@gmail.com>
2024-07-04 00:09:18 -0400

    fix note

--- database/migrations/2024_07_03_184803_add_note_to_contas_table.php
+++ database/migrations/2024_07_03_184803_add_note_to_contas_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('contas', function (Blueprint $table) {
-            $table->text('note')->after('user_id');
+            $table->text('note')->after('user_id')->nullable();
         });
     }
 
