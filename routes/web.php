e158a5c802d8dac3bc71fd66340354f8f76704d2
hadson moraes <hadsonmartins10@gmail.com>
2024-07-10 20:06:51 -0400

    alterar nome das rotas

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::get('/cadastro/create', [HomeController::class, 'create'])->name('contas.create');
     Route::post('/cadastro', [HomeController::class, 'store'])->name('contas.store');
-    Route::get('/cadastro/{id}', [HomeController::class, 'show'])->name('contas.show');
-    Route::get('/cadastro/edit/{id}', [HomeController::class, 'edit'])->name('contas.edit');
-    Route::put('/cadastro/update/{id}', [HomeController::class, 'update'])->name('contas.update');
-    Route::delete('/cadastro/delete/{id}', [HomeController::class, 'destroy'])->name('contas.destroy');
+    Route::get('/contas/{id}', [HomeController::class, 'show'])->name('contas.show');
+    Route::get('/contas/edit/{id}', [HomeController::class, 'edit'])->name('contas.edit');
+    Route::put('/contas/update/{id}', [HomeController::class, 'update'])->name('contas.update');
+    Route::delete('/contas/delete/{id}', [HomeController::class, 'destroy'])->name('contas.destroy');
     Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
 
     Route::get('/profile/edit/{id}', [ProfileController::class, 'edit'])->name('profile.edit');
